12361235c02452c240e33c8e5800018f3397815a
Bilal Baysal <bilalbaysal@outlook.com>
2023-12-16 14:25:55 +0300

    BB-161223
    FrontEnd done.

---
+++ LoginScreenWithIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LoginScreenWithIdentity.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
